ef20d0a11d451ebcb70bb6caf94f63ed3ccc2aae
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 22:37:46 +0000

    Implemented testing for asyncBreeds.js

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    if (!error) callback(data);
+    if (error) {
+      callback(undefined);
+      return;
+    }
+    callback(data);
   });
 };
 
-const printReturnValue = function(returnValue) {
-  console.log('Return Value:', returnValue);
-};
-
-breedDetailsFromFile('Bombay', printReturnValue);
+module.exports = breedDetailsFromFile;
